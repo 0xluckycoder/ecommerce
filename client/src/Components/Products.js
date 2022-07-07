b9eb379777c3e3dbee9e8ba3e94c058bd1a0da3f
0xlakshan <lakshanperera.dev@gmail.com>
2022-07-07 21:30:06 +0530

    add products modal prototype

--- client/src/Components/Products.js
+++ client/src/Components/Products.js
@@ -1,14 +1,24 @@
+import React, { useState } from 'react';
 import productDemo from '../assets/product-demo.png';
 import styles from './products.module.scss';
-import { Col, Row, Button } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Col, Row, Button, Input } from 'antd';
+
+const { TextArea } = Input;
 
 export default function Products() {
+
+    const [modal, setModal] = useState(false);
+
     return (
         <div className="products">
             <div className={`${styles.productsInner} products-inner`}>
                 <h2>Products</h2>
+                {/* modal */}
+                {modal && <Modal setModal={setModal} />}
                 <Row className={styles.rowSection}>
-                    <Button className="themed-btn">Add New</Button>
+                    <Button className="themed-btn" onClick={() => setModal(true)}>Add New</Button>
                 </Row>
                 <Row gutter={32} className={styles.rowSection}>
                     <Col lg={6} md={12} sm={24} xs={24}>
@@ -42,5 +52,44 @@ export default function Products() {
                 </Row>
             </div>
         </div>
+    );
+}
+
+function Modal({ setModal }) {
+    return (
+        <section className={styles.modal}>
+        <div className={styles.close}>
+            <FontAwesomeIcon icon={faXmark} onClick={() => setModal(false)} />
+        </div>
+        <h2>Edit Product</h2>
+        <Button className={`${styles.uploadBtn} themed-btn`}>Upload</Button>
+        <img />
+        <img />
+        <img />
+        <form className={styles.modalForm}>
+            <div className={styles.formRow}>
+                <label>Product Name</label>
+                <Input />
+            </div>
+            <div className={styles.inlineFormRow}>
+            <div className={styles.formRow}>
+                    <label>Stock</label>
+                    <Input />
+            </div>
+            <div className={styles.formRow}>
+                    <label>Price</label>
+                    <Input />
+                </div>
+            </div>
+            <div className={styles.formRow}>
+                <label>Description</label>
+                <TextArea rows={4} />
+            </div>
+            <div className={styles.buttonWrapper}>
+                <Button className="themed-btn">Submit</Button>
+                <Button className="themed-btn">Remove</Button>
+            </div>
+        </form>
+    </section>
     );
 }
