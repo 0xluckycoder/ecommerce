a669d1f4104c8f1df7606bb9445a6916d7e3dd25
0xluckycoder <lakshanperera.dev@gmail.com>
2022-08-26 00:41:50 +0530

    uploaded to s3 / ACL permissions needs be added

--- server/src/services/vendorService.js
+++ server/src/services/vendorService.js
@@ -1,8 +1,10 @@
-const vendor = require('../database/vendor');
 const sharp = require('sharp');
 const path = require('path');
 const fs = require('fs');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+
 const customError = require('../utils/customError');
+const vendor = require('../database/vendor');
 
 const createVendor = async (user) => {
     try {
@@ -59,8 +61,57 @@ const uploadLogo = async (file, userId) => {
 
         // remove initially uploaded image
         fs.unlinkSync(file.path);
+        
+        // console.log(data);
 
-        console.log(data);
+        const client = new S3Client({
+            region: process.env.AWS_COGNITO_REGION,
+            credentials : {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+            },
+        });
+
+        const fileStream = fs.createReadStream(`${uploadDestination}.webp`);
+
+        // console.log('🔥', `${uploadDestination}.webp`);
+        // console.log(fileStream);
+
+        const putObjectCommand = new PutObjectCommand({
+            Body: fileStream,
+            Key: `${fileNameWithoutExt}.webp`,
+            Bucket: process.env.AWS_S3_BUCKET_NAME
+        });
+
+        const putObjectCommandResponse = await client.send(putObjectCommand);
+
+        console.log(putObjectCommandResponse);
+
+        /*
+        arn:aws:s3:::freebiemedia
+        https://freebiemedia.s3.ap-south-1.amazonaws.com/33a5a4adf5f57c15be701e9a3d28717e221d791d.jpg
+        const parallellUploads = new Upload({
+        client: new S3Client({
+            credentials: {
+            accessKeyId: process.env.S3_KEY,
+            secretAccessKey: process.env.S3_SECRET,
+            },
+            region: process.env.S3_REGION,
+            endpoint: "http://us-east-1.linodeobjects.com",
+        }),
+        params: {
+            Bucket: process.env.S3_BUCKET,
+            Key: "gauntlet_" + uuid() + "_" + files.files.name,
+            Body: fs.createReadStream(files.files.path),
+        },
+        });
+
+        parallellUploads.on("httpUploadProgress", (progress) => {
+        console.log(progress);
+        });
+
+        await parallellUploads.done();
+        */ 
 
         /*
             - read more about pricing
